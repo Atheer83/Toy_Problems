58597395d49d7ee1e6635257c5f0f77b745d36ed
Atheer83 <atheer.for.tech@gmail.com>
2018-02-25 09:35:41 +0200

    Toy problem for Sundy

--- strings.js
+++ strings.js
@@ -2,7 +2,15 @@
 // 	Input :: "a"
 // 	Ouput :: "Position of alphabet: 1"
 	function characPosit(character){
-		//your code is here
+		var alphabet = "abcdefghijklmnopqrstuvwxyz";
+		var position = 1;
+		for (var i = 0; i < alphabet.split("").length; i++) {
+			if (character.toLowerCase() === alphabet.split("")[i]) {
+				position = position + i;
+			}
+		}
+
+	return "Position of alphabet: " + position;		
 	}
 
 // Write a function called repeatStr which repeats the given
@@ -10,5 +18,7 @@
 // repeatStr("hello",4) ==> 'hellohellohellohello'
 	
 	function repeatStr (n, s) {
-		//your code is here
+		if (s === 1) {
+			return n;
+		} return n + repeatStr(n, s-1);
 	}
