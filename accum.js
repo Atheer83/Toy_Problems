cf01bfd7c11513a00c1c455d84d679256f6b1f55
Atheer83 <atheer.for.tech@gmail.com>
2018-02-27 09:19:13 +0200

    I used caalback function

--- accum.js
+++ accum.js
@@ -4,6 +4,20 @@
 // accum("RqaEzty"); // "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
 // accum("cwAt");    // "C-Ww-Aaa-Tttt"
 
+function counting (n, s) {
+		if (s === 1) {
+			return n;
+		} return n + counting(n, s-1);
+	}
+
     function accum(input){
-        //your code is here
+        var str = input[0].toUpperCase();
+        	for (var i = 0; i < input.length; i++) {
+        		if (i > 0) {
+        		str = str + "-" + input[i].toUpperCase() + counting(input[i], i).toLowerCase() ;
+
+        		}
+        		//str = str + input[i].toUpperCase();
+        	}
+        return str;
     }
