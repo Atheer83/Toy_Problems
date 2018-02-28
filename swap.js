a0f2117e0cc049eec19b65b97afa4300e8dff656
Atheer83 <atheer.for.tech@gmail.com>
2018-02-28 09:36:11 +0200

    Swap Function

--- swap.js
+++ swap.js
@@ -2,5 +2,13 @@
 	// IbrAHiM --> iBRahIm
 	// ToYPRoblEm --> tOyprOBLeM
 	function swap(input){
-		//your code is here
+		var	newStr = "";
+		for (var i = 0; i < input.length; i++) {
+			if (input[i].charCodeAt() < 91 ) {
+				newStr = newStr + input[i].toLowerCase();
+			} 
+			if (input[i].charCodeAt() > 91 )
+			newStr = newStr + input[i].toUpperCase()
+		}
+		return newStr;
 	}
